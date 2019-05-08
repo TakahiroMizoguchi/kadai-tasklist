0bb8827a9d0cd2088fca5ba4e6cb5d78f5ce6806
takahiro.mizoguchi <miso3313@gmail.com>
2019-05-08 05:06:14 +0000

    login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,13 @@
 module SessionsHelper
   def current_user
+    # 既に現在のログインユーザが代入されていたら何もせず
+    # 代入されていなかったら User.find(...) からログインユーザを取得し
+    # @current_user に代入する処理を1行で書いたもの
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
+    # ユーザが現在ログインしているかを調べる
     !!current_user
   end
 end
